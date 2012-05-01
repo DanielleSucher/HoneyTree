d5ee109ad613fc33bbcb50a70dd2e64d6cbf1fa3
Danielle Sucher <dsucher@gmail.com>
2012-05-01 02:41:15 -0400

    Added mbr query and haversine function to identify nearby trees

--- honeytree-lib.rb
+++ honeytree-lib.rb
@@ -1,5 +1,6 @@
 require 'mysql2'
 require 'csv'
+require 'pp'
 
 class Honeytree
 
@@ -27,20 +28,69 @@ class Honeytree
 		self.import_csv file
 	end
 
-	def set_polygon(point)
-		@client.query "SET @poly = 'Polygon((30000 15000,
-				                             31000 15000,
-				                             31000 16000,
-				                             30000 16000,
-				                             30000 15000))';"
-		# Those are temporary numbers as a placeholder
-		# TODO make this function actually work!
+	# def set_polygon(numbersides, point, radius)
+	# 	# 1 mile = 1/69 degree latitude
+	# 	# 1 mile = 1/ (69 * cos(latitude)) degrees longitude
+	# 	x = point[0].to_i
+	# 	y = point[1].to_i
+	# 	lat_r = radius/69.0
+	# 	lon_r = radius / (69.0 * Math.cos(radians(x)))
+	# 	vertices = "#{x} #{y + lon_r}"
+	# 	theta = (Math::PI * 2) / numbersides
+	# 	i = 1
+	# 	(numbersides - 1).times do 
+	# 		vertices.concat(", #{x + lat_r * Math.sin(i * theta)} #{y + lon_r * Math.cos(i * theta)}")
+	# 		i += 1
+	# 	end
+	# 	vertices.concat(", #{x} #{y} + lon_r}")
+	# 	"'Polygon((#{vertices}))'"
+	# end
+
+	def find_trees_in_square(p, miles)
+		x1 = p[0] + miles / 69.0
+		y1 = p[1] + miles / (69.0 / Math.cos(self.radians(p[0])))
+		x2 = p[0] - miles / 69.0
+		y2 = p[1] - miles / (69.0 / Math.cos(self.radians(p[0])))
+		line = "LineString(GeomFromText('POINT(#{x1} #{y1})'), GeomFromText('POINT(#{x2} #{y2})'))"
+		@client.query "SELECT species, AsText(latlong) FROM trees WHERE MBRContains(#{line}, latlong);"
 	end
 
-	def find_nearby_trees
-		@client.query "SELECT AsText(latlong), species FROM trees WHERE
-					   MBRContains(GeomFromText(@poly),latlong);"
+	def haversine (point1,point2)
+	    earth_radius = 3956
+	    lat1 = self.radians(point1[0])
+	    lon1 = self.radians(point1[1])
+	    lat2 = self.radians(point2[0])
+	    lon2 = self.radians(point2[1])
+	    dlat = lat2 - lat1
+	    dlon = lon2 - lon1
+	    a = (Math.sin(dlat/2.0))**2 + Math.cos(lat1) * Math.cos(lat2) * (Math.sin(dlon/2.0))**2
+		great_circle_distance = 2 * Math.asin([1, Math.sqrt(a)].min)
+
+	    d = earth_radius * great_circle_distance
 	end
 
+	def find_nearby_trees(point, miles)
+		p = point.split(" ")
+		p.map! { |x| x = x.to_f }
+		m = miles.to_f
+		results = self.find_trees_in_square(p, m)
+		trees = {}
+		results.each do |row|
+			pt = row["AsText(latlong)"].gsub(/[^\d\s\.-]/, "").split(" ").map! { |x| x = x.to_f }
+			dist = haversine(p,pt)
+			puts dist
+			if dist <= m
+				unless row['species'].size <= 1
+					trees[row['species']] ||= 0
+					trees[row['species']] += 1
+				end
+			end
+		end
+		return trees
+	end
+
+	def radians(degrees)
+		degrees * Math::PI / 180
+	end
 end
 
--- honeytree.rb
+++ honeytree.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/env ruby
 
+$:.unshift File.expand_path('.')
+
 require 'mysql2'
 require 'csv'
 require 'honeytree-lib'
 require 'optparse'
+require 'pp'
 
 ht = Honeytree.new
+
+puts ht.find_nearby_trees("40.6223262 -73.955483", "1") # EMJC, of course
+# results.each do |row|
+# 	puts row
+# 	puts row['species']
+# end
