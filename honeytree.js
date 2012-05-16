cb78b7a41a30d0879fbdf0de41061f243b65f20c
Danielle Sucher <dsucher@gmail.com>
2012-05-16 11:07:53 -0400

    Updated README and refactored a tad

--- honeytree.js
+++ honeytree.js
@@ -1,39 +1,3 @@
-function drawDonut(data) {
-    var w = 400,
-        h = 400,
-        r = Math.min(w, h) / 2,
-        labelr = r + 30, // radius for label anchor
-        color = d3.scale.category20c(),
-        donut = d3.layout.pie(),
-        arc = d3.svg.arc().innerRadius(r * 0.6).outerRadius(r),
-        pos = d3.svg.arc().innerRadius(r + 38).outerRadius(r + 38);
-
-    var vis = d3.select("#chart")
-        .append("svg:svg")
-        .data([data])
-        .attr("width", w + 300)
-        .attr("height", h + 50);
-
-    var arcs = vis.selectAll("g.arc")
-        .data(donut.value(function(d) { return d.val; }))
-        .enter().append("svg:g")
-        .attr("class", "arc")
-        .attr("transform", "translate(" + (r + 150) + "," + r + ")");
-
-    arcs.append("svg:path")
-        .attr("fill", function(d, i) { return color(i); })
-        .attr("d", arc);
-
-    arcs.append("text")
-        .attr("transform", function(d) { return "translate(" + pos.centroid(d) + ")"; })
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .attr("display", function(d) { return d.value > 3.0 ? null : "none"; })
-        .text(function(d, i) {
-            return d.data.name;
-        });
-}
-
 function inputFocus(i){
     if(i.value=="enter your hive's NYC street address"){ i.value="";}
 }
@@ -91,6 +55,7 @@ function drawTree(data) {
     .attr("d", diagonal)
     .style("stroke", "#633A11")
     .style("stroke-width", 3)
+    .style("stroke-opacity", 0.7)
     .style("fill", "none");
 
     var node = vis.selectAll("g.node")
